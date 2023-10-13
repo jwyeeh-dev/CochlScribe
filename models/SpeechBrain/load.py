ec851210e077700889cae5052d3c94812fcea392
Jaeyeong Hwang <99489807+jwyeeh-dev@users.noreply.github.com>
2023-10-13 05:26:02 +0000

    Update load.py
    - change extracted audio path.

--- models/SpeechBrain/load.py
+++ models/SpeechBrain/load.py
@@ -2,10 +2,13 @@ from speechbrain.pretrained.interfaces import foreign_class
 from speechbrain.pretrained.interfaces import Speech_Emotion_Diarization
 
 def speechMood(audio):
+    # If you want to change your extracted audio file, change the audio path below
+    audio_path = './dataset/audio/extracted_audio.mp3'
+
     classifier = Speech_Emotion_Diarization.from_hparams(
         source="speechbrain/emotion-diarization-wavlm-large"
     )
 
     diary = classifier.diarize_file(audio)
     
-    return diary
+    return diary[audio_path]
